af89f9dba73677ac5617f12321f8e38a068733a4
Kjell Maekelberg <maekelbergkjell@gmail.com>
2026-03-27 23:07:53 +0100

    perf: replace 1.5GB pokemon-sprites package with CDN proxy
    Sprites are now fetched from GitHub's CDN on demand and cached
    for 7 days. Removes the massive PokeAPI/sprites git dependency
    that was causing build timeouts.

--- src/routes/assets/img/pokemon/[variant]-[id].[type]/+server.js
+++ src/routes/assets/img/pokemon/[variant]-[id].[type]/+server.js
@@ -1,45 +1,23 @@
-let pokemon = import.meta.glob(
-  [
-    '/node_modules/pokemon-sprites/sprites/pokemon/*.png',
-    '/node_modules/pokemon-sprites/sprites/pokemon/shiny/*.png',
-  ],
-  {
-    query: '?base64',
-    import: 'default'
-  }
-)
-
-const keyToBase64 = async (spriteName, shiny) => {
-  let defaultShinyPath = `/node_modules/pokemon-sprites/sprites/pokemon/shiny/${spriteName}.png`
-  let defaultPath = `/node_modules/pokemon-sprites/sprites/pokemon/${spriteName}.png`;
-
-  let sprite = null;
-
-  if(shiny) {
-    sprite = pokemon[defaultShinyPath];
-  } else {
-    sprite = pokemon[defaultPath];
-  }
-
-  if(!sprite) return null;
-
-  return await sprite();
-}
+const SPRITE_CDN = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon'
 
 export async function GET({ params }) {
+  const { id, variant } = params
 
-  const {id, variant} = params;
+  const path = variant === 'shiny' ? `shiny/${id}` : id
+  const url = `${SPRITE_CDN}/${path}.png`
 
-  let shiny = false;
-  if(variant === 'shiny') {
-    shiny = true;
+  const res = await fetch(url)
+
+  if (!res.ok) {
+    return new Response(null, { status: 404 })
   }
 
-  let sprite = await keyToBase64(id, shiny);
+  const buffer = await res.arrayBuffer()
 
-  return new Response(Buffer.from(sprite, 'base64'), {
+  return new Response(buffer, {
     headers: {
-      'Content-Type': 'image/png'
+      'Content-Type': 'image/png',
+      'Cache-Control': 'public, max-age=604800, immutable'
     }
-  });
+  })
 }
